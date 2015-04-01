232c99ed662ee76d7d6e00683cee8642f0d87f6f
Ciechom <ciechom.ziom@gmail.com>
2015-04-01 16:03:02 +0200

    Added output message

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,28 @@ public class Main {
     public static void main(String[] args) {
         int n;
         if(System.getProperty("user.language").equalsIgnoreCase("pl")){
-            n = Integer.parseInt(JOptionPane.showInputDialog("Podaj liczbe"));
+            n=Integer.parseInt(JOptionPane.showInputDialog("Podaj liczbe"));
         }else{
-            n = Integer.parseInt(JOptionPane.showInputDialog("Write your number"));
+            n=Integer.parseInt(JOptionPane.showInputDialog("Write your number"));
         }
-        int kroki = 0;
+        int kroki=0, x=n;
 
-        while (n > 1) {
-            if (n % 2 == 0) {
-                n = n / 2;
-            } else {
-                n = n * 3;
-                n = n + 1;
+        while(n>1){
+            if(n%2==0){
+                n=n/2;
+            }else{
+                n=n*3; n=n+1;
             }
             kroki++;
         }
-        System.out.println(kroki);
+        String toShow;
+        if(System.getProperty("user.language").equalsIgnoreCase("pl")){
+            toShow = "Liczba krokow potrzebna aby osiagnac 1: "+String.valueOf(kroki)+" przy uzyciu liczby: "+x;
+        }else{
+            toShow = "It takes: "+String.valueOf(kroki)+" steps to reach 1 using number: "+x;
+        }
+
+        JOptionPane.showMessageDialog(null, toShow);
+
     }
 }
